9c7e4f0fce1f1b6a660cd491d8634446eb8185cf
Jones_Ryan <joner689@hsd.k12.or.us>
2021-11-04 11:00:23 -0700

    Fixed makeWindow function and made a plan for what to do. Just adding user friendliness until I can install modules

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 def makeWindow(screenName, dimensionX, dimensionY, bgColor):
-    root = tk.Tk(screenName)
+    root = tk.Tk()
+    root.title(screenName)
     root.configure(background=bgColor)
     root.geometry("%dx%d+0+0" % (dimensionX, dimensionY))
     root.mainloop()
 
 def main():
-    makeWindow("Test", 50, 50, "white")
+    makeWindow("Web Finder Beta", 1500, 800, "white")
 
 if __name__ == "__main__":
     import tkinter as tk
--- miniTests.py
+++ miniTests.py
@@ -1,3 +1,8 @@
 #Testing to see what vscode has to offer as far as imports go
 #import pygame doesn't work
-from tkinter import tk as tk
+import tkinter as tk
+root = tk.Tk()
+root.title("Hi")
+root.configure(background="white")
+root.geometry("%dx%d+0+0" % (100, 200))
+root.mainloop()
