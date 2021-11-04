dd4d3ae6c8878e9d4208bd27a87bde8bb713ca59
Jones_Ryan <joner689@hsd.k12.or.us>
2021-11-04 11:11:02 -0700

    Added start of userSettings function to allow user to format tkinter window

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
+from tkinter.constants import HORIZONTAL
+
+
 def makeWindow(screenName, dimensionX, dimensionY, bgColor):
+    global root
     root = tk.Tk()
     root.title(screenName)
     root.configure(background=bgColor)
     root.geometry("%dx%d+0+0" % (dimensionX, dimensionY))
-    root.mainloop()
+
+def userSettings():
+    xSlide = Scale(root, from_=0, to=42)
+    xSlide.pack()
+    ySlide = Scale(root, from_=0, to=200, orient=HORIZONTAL)
+    ySlide.pack()
 
 def main():
     makeWindow("Web Finder Beta", 1500, 800, "white")
+    userSettings()
 
 if __name__ == "__main__":
     import tkinter as tk
+    from tkinter import Scale
     main()
 
+root.mainloop()
--- miniTests.py
+++ miniTests.py
@@ -1,8 +1,20 @@
 #Testing to see what vscode has to offer as far as imports go
 #import pygame doesn't work
+
+# import tkinter as tk
+# root = tk.Tk()
+# root.title("Hi")
+# root.configure(background="white")
+# root.geometry("%dx%d+0+0" % (100, 200))
+# root.mainloop()
+
 import tkinter as tk
+from tkinter import Scale
+
 root = tk.Tk()
-root.title("Hi")
-root.configure(background="white")
-root.geometry("%dx%d+0+0" % (100, 200))
+xSlide = Scale(root, from_=0, to=42)
+xSlide.pack()
+ySlide = Scale(root, from_=0, to=200)
+ySlide.pack()
+
 root.mainloop()
