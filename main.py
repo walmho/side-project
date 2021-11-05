fe5db433b3b569abd550b8c935ff5e6b6a8e5772
walmho <jones.ryan.t.01@gmail.com>
2021-11-05 08:30:45 -0700

    All heck broke loose

--- main.py
+++ main.py
@@ -10,21 +10,27 @@ def makeWindow(screenName, dimensionX, dimensionY, bgColor):
 
 def userSettings():
     global xSlide
-    xSlide = Scale(root, from_=0, to=42)
+    xSlide = Scale(root, from_=0, to=42, command=applyChanges)
     xSlide.pack()
     global ySlide
-    ySlide = Scale(root, from_=0, to=200, orient=HORIZONTAL)
+    ySlide = Scale(root, from_=0, to=200, orient=HORIZONTAL, command=applyChanges)
     ySlide.pack()
 
 def applyChanges(x, y, bgColor="red"):
     root.geometry("%dx%d+0+0" % (x.get(), y.get()))
     root.configure(background=bgColor)
+    print(x)
+    print(y)
 
 def makeButtons():
-    editLayout = Button(root, text="Settings", command=lambda:applyChanges(xSlide, ySlide))
+    showSettings = Button(root, text="Settings", command=lambda:userSettings())
+    showSettings.pack()
+    finishChange = Button(root, text="Save Changes", command=lambda:applyChanges)
+    finishChange.pack()
 
 def main():
-    makeWindow("Web Finder Beta", 1500, 800, "white")
+    makeWindow("Web Finder Beta", 1000, 600, "white")
+    makeButtons()
     userSettings()
 
 if __name__ == "__main__":
