0618ba5979c1574acff7b051cea3dd7da79e4a65
walmho <jones.ryan.t.01@gmail.com>
2021-11-05 08:45:32 -0700

    All heck is still loose, slowly fixing - added comments

--- main.py
+++ main.py
@@ -8,7 +8,14 @@ def makeWindow(screenName, dimensionX, dimensionY, bgColor):
     root.configure(background=bgColor)
     root.geometry("%dx%d+0+0" % (dimensionX, dimensionY))
 
+#Sub-window for settings
 def userSettings():
+    #Sub-window setup
+    global settings
+    settings = tk.Tk()
+    settings.title("Settings")
+
+    #Components within settings window
     global xSlide
     xSlide = Scale(root, from_=0, to=42, command=applyChanges)
     xSlide.pack()
@@ -16,21 +23,15 @@ def userSettings():
     ySlide = Scale(root, from_=0, to=200, orient=HORIZONTAL, command=applyChanges)
     ySlide.pack()
 
+#Button command to apply changes
 def applyChanges(x, y, bgColor="red"):
     root.geometry("%dx%d+0+0" % (x.get(), y.get()))
     root.configure(background=bgColor)
     print(x)
     print(y)
 
-def makeButtons():
-    showSettings = Button(root, text="Settings", command=lambda:userSettings())
-    showSettings.pack()
-    finishChange = Button(root, text="Save Changes", command=lambda:applyChanges)
-    finishChange.pack()
-
 def main():
     makeWindow("Web Finder Beta", 1000, 600, "white")
-    makeButtons()
     userSettings()
 
 if __name__ == "__main__":
