420c1138eb579573dc54be644950fffd9e06efab
walmho <jones.ryan.t.01@gmail.com>
2021-11-10 22:19:01 -0800

    Added more comments and color changes

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+PINK = (255, 0, 255)
+PURPLE = (155, 0, 255)
+
+#Colorlist exludes white for now because it's kind of difficult to spot a white object against white BG
+colorList = [BLACK, RED, GREEN, BLUE, YELLOW, PINK, PURPLE]
 
 #Clock setup
 clock = pygame.time.Clock()
@@ -29,6 +35,8 @@ class gridSquare():
         self.coord = [0, 0]
         self.size = size
         self.movespeed = 1
+        self.spriteColor = colorList[0]
+        self.colorCount = 0
         #Basically, this makes the var windowsize reference whichever axis is smaller, so the sprite knows when its out of bounds
         if dimensions[0] < dimensions[1]:
             self.windowsize = dimensions[0]
@@ -37,7 +45,7 @@ class gridSquare():
 
     def update(self):
         #Draws a cube using top left coordinate as reference and wanted size
-        pygame.draw.polygon(window, BLACK, (
+        pygame.draw.polygon(window, self.spriteColor, (
             (self.coord[0], self.coord[1]),
             (self.coord[0]+self.size, self.coord[1]), 
             (self.coord[0]+self.size, self.coord[1]+self.size), 
@@ -50,11 +58,15 @@ class gridSquare():
         if self.coord[1] > self.windowsize-self.size:
             self.coord = [0, 0]
 
-    def isOver(self):
+    def ifClicked(self):
         # Checks if the mouse is over the box relative to its current position
         pos = pygame.mouse.get_pos()
         if pos >= (self.coord[0], self.coord[1]) and pos <= (self.coord[0]+self.size, self.coord[1]+self.size):
             print("Clicked!")
+            self.colorCount += 1
+            if self.colorCount >= len(colorList):
+                self.colorCount = 0
+            self.spriteColor = colorList[self.colorCount]
         elif pos < (self.coord[0], self.coord[1]) and pos > (self.coord[0]+self.size, self.coord[1]+self.size):
             print("Not clicked!")
 
@@ -73,7 +85,7 @@ def gameLoop():
                 sys.exit()
             #If mouse button is clicked:
             if event.type == MOUSEBUTTONUP:
-                square.isOver()
+                square.ifClicked()
 
         square.update()
         pygame.display.update()
